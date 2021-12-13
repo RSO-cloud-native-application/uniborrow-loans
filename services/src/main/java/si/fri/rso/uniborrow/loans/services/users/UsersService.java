877bd64df1211ccdc94e19e520b11d652c2642c4
Kevin Stampar <clovek.kevin@gmail.com>
2021-12-13 15:59:47 +0100

    Implementing users

--- services/src/main/java/si/fri/rso/uniborrow/loans/services/users/UsersService.java
+++ services/src/main/java/si/fri/rso/uniborrow/loans/services/users/UsersService.java
@@ -1,11 +1,21 @@
 package si.fri.rso.uniborrow.loans.services.users;
 
+import com.kumuluz.ee.discovery.annotations.DiscoverService;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 
 @ApplicationScoped
 public class UsersService {
+
+    @Inject
+    @DiscoverService(value = "uniborrow-users", environment = "dev", version = "1.0.0")
+    private WebTarget webTarget;
+
     public boolean checkUserExists(String userId) {
-        System.out.println("We got a call bby");
+        webTarget.path("/v1/users").request(MediaType.APPLICATION_JSON);
         return true;
     }
 }
