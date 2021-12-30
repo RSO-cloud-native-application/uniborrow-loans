59a8eb1d35653502c69e8c13648fbb2fe8a8d80a
Kevin Stampar <clovek.kevin@gmail.com>
2021-12-30 22:35:28 +0100

    Fixed service discovery for user service.

--- services/src/main/java/si/fri/rso/uniborrow/loans/services/users/UsersService.java
+++ services/src/main/java/si/fri/rso/uniborrow/loans/services/users/UsersService.java
@@ -4,7 +4,6 @@ import com.kumuluz.ee.discovery.annotations.DiscoverService;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -17,7 +16,7 @@ public class UsersService {
 
     @Inject
     @DiscoverService(value = "uniborrow-users-service", environment = "dev", version = "1.0.0")
-    private WebTarget webTarget = ClientBuilder.newClient().target("http://uniborrow-users:8080");
+    private WebTarget webTarget;
 
     public boolean checkUserExists(Integer userId) {
         Response userCheck = webTarget.path("/v1/users").path(userId.toString()).request(MediaType.APPLICATION_JSON).buildGet().invoke();
