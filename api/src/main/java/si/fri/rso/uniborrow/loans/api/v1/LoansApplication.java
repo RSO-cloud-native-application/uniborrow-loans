df678ad6d92b61868dd56ed364730bf5eb6ee240
Kevin Stampar <clovek.kevin@gmail.com>
2022-01-09 13:21:49 +0100

    Added openapi.

--- api/src/main/java/si/fri/rso/uniborrow/loans/api/v1/LoansApplication.java
+++ api/src/main/java/si/fri/rso/uniborrow/loans/api/v1/LoansApplication.java
@@ -1,10 +1,25 @@
 package si.fri.rso.uniborrow.loans.api.v1;
 
 import com.kumuluz.ee.discovery.annotations.RegisterService;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Uniborrow Loans API",
+                version = "v1",
+                contact = @Contact(email = "ks0466@student.uni-lj.si"),
+                license = @License(name = "dev"),
+                description = "API for managing loans for Uniborrow application."
+        ),
+        servers = @Server(url = "http://35.223.79.242/uniborrow-loans/")
+)
 @RegisterService
 @ApplicationPath("/v1")
 public class LoansApplication extends Application {
--- api/src/main/java/si/fri/rso/uniborrow/loans/api/v1/resources/LoansDataResource.java
+++ api/src/main/java/si/fri/rso/uniborrow/loans/api/v1/resources/LoansDataResource.java
@@ -1,6 +1,14 @@
 package si.fri.rso.uniborrow.loans.api.v1.resources;
 
 import com.kumuluz.ee.logs.cdi.Log;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import si.fri.rso.uniborrow.loans.models.entities.AcceptedState;
 import si.fri.rso.uniborrow.loans.models.entities.LoanEntity;
 import si.fri.rso.uniborrow.loans.services.beans.LoansDataProviderBean;
@@ -40,6 +48,18 @@ public class LoansDataResource {
     protected UriInfo uriInfo;
 
     @GET
+    @Operation(description = "Get loans by filter, or all.", summary = "Get loans by filter, or all.")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Loans that fit the filter.",
+                    content = @Content(schema = @Schema(implementation = LoanEntity.class, type = SchemaType.ARRAY))
+            ),
+            @APIResponse(
+                    responseCode = "404",
+                    description = "No loans found."
+            )
+    })
     public Response getAllLoans() {
         List<LoanEntity> loanEntities = loansDataProviderBean.getLoansFilter(uriInfo);
         return Response.status(Response.Status.OK).entity(loanEntities).build();
@@ -47,14 +67,48 @@ public class LoansDataResource {
 
     @GET
     @Path("/{loanId}")
-    public Response getLoan(@PathParam("loanId") Integer loanId) {
+    @Operation(description = "Get loan by id.", summary = "Get loan by id.")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Loan by id.",
+                    content = @Content(schema = @Schema(implementation = LoanEntity.class))
+            ),
+            @APIResponse(
+                    responseCode = "404",
+                    description = "Loan with id not found."
+            )
+    })
+    public Response getLoan(@Parameter(
+            description = "Loan id.",
+            required = true
+    ) @PathParam("loanId") Integer loanId) {
         LoanEntity loanEntity = loansDataProviderBean.getLoan(loanId);
         return Response.status(Response.Status.OK).entity(loanEntity).build();
     }
 
     @POST
     @Path("/{loanId}/accept")
-    public Response acceptLoan(@PathParam("loanId") Integer loanId) {
+    @Operation(description = "Accept loan with id.", summary = "Accept loan with id.")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Accepted loan",
+                    content = @Content(schema = @Schema(implementation = LoanEntity.class))
+            ),
+            @APIResponse(
+                    responseCode = "417",
+                    description = "Problems with sending cash."
+            ),
+            @APIResponse(
+                    responseCode = "406",
+                    description = "User does not have enough cash."
+            )
+    })
+    public Response acceptLoan(@Parameter(
+            description = "Loan id.",
+            required = true
+    ) @PathParam("loanId") Integer loanId) {
         LoanEntity loan = loansDataProviderBean.getLoan(loanId);
         if (loan == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
@@ -76,7 +130,22 @@ public class LoansDataResource {
 
     @POST
     @Path("/{loanId}/reject")
-    public Response rejectLoan(@PathParam("loanId") Integer loanId) {
+    @Operation(description = "Reject loan with id.", summary = "Reject loan with id.")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Rejected loan",
+                    content = @Content(schema = @Schema(implementation = LoanEntity.class))
+            ),
+            @APIResponse(
+                    responseCode = "404",
+                    description = "Loan not found."
+            )
+    })
+    public Response rejectLoan(@Parameter(
+            description = "Loan id.",
+            required = true
+    ) @PathParam("loanId") Integer loanId) {
         LoanEntity rejectedLoan = loansDataProviderBean.rejectLoan(loanId);
         if (rejectedLoan == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
@@ -86,7 +155,25 @@ public class LoansDataResource {
 
     @POST
     @Path("/propose")
-    public Response proposeLoan(LoanEntity loanEntity) {
+    @Operation(description = "Propose new loan.", summary = "Propose new loan.")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Proposed loan",
+                    content = @Content(schema = @Schema(implementation = LoanEntity.class))
+            ),
+            @APIResponse(
+                    responseCode = "400",
+                    description = "Problems with request body."
+            )
+    })
+    public Response proposeLoan(
+            @RequestBody(
+                    description = "Proposed loan info.",
+                    required = true,
+                    content = @Content(schema = @Schema(implementation = LoanEntity.class))
+            )
+                    LoanEntity loanEntity) {
         loanEntity.setAcceptedState(AcceptedState.PENDING);
         if (loanEntity.getFromId() == null || loanEntity.getStartTime() == null ||
                 loanEntity.getItemId() == null || loanEntity.getToId() == null || loanEntity.getEndTime() == null ||
@@ -102,8 +189,28 @@ public class LoansDataResource {
 
     @PATCH
     @Path("{loanId}")
-    public Response patchLoan(@PathParam("loanId") Integer loanId,
-                              LoanEntity loanEntity) {
+    @Operation(description = "Patch a loan.", summary = "Patch a loan.")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Patched loan",
+                    content = @Content(schema = @Schema(implementation = LoanEntity.class))
+            ),
+            @APIResponse(
+                    responseCode = "400",
+                    description = "Problems with request body."
+            )
+    })
+    public Response patchLoan(@Parameter(
+            description = "Loan id.",
+            required = true
+    ) @PathParam("loanId") Integer loanId,
+                              @RequestBody(
+                                      description = "Changed loan info.",
+                                      required = true,
+                                      content = @Content(schema = @Schema(implementation = LoanEntity.class))
+                              )
+                                      LoanEntity loanEntity) {
         if (loanEntity.getFromId() != null && !usersService.checkUserExists(loanEntity.getFromId()) ||
                 (loanEntity.getToId() != null && !usersService.checkUserExists(loanEntity.getToId())) || !itemsService.checkItemAvailable(loanEntity.getItemId())) {
             return Response.status(Response.Status.BAD_REQUEST).build();
@@ -117,10 +224,24 @@ public class LoansDataResource {
 
     @DELETE
     @Path("{loanId}")
-    public Response deleteLoan(@PathParam("loanId") Integer loanId) {
+    @Operation(description = "Delete a loan.", summary = "Delete a loan.")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Loan deleted."
+            ),
+            @APIResponse(
+                    responseCode = "404",
+                    description = "Loan not found."
+            )
+    })
+    public Response deleteLoan(@Parameter(
+            description = "Loan id.",
+            required = true
+    ) @PathParam("loanId") Integer loanId) {
         boolean deleted = loansDataProviderBean.deleteLoan(loanId);
         if (deleted) {
-            return Response.status(Response.Status.NO_CONTENT).build();
+            return Response.status(Response.Status.OK).build();
         } else {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
